4c639e255d617752848877d7db7b1bd1232ae699
Joe <820839844@qq.com>
2017-11-20 20:45:25 +0800

    client.js updated

--- face2face/client.js
+++ face2face/client.js
@@ -31,3 +31,31 @@ function send(message){
     }
     connection.send(JSON.stringify(message));
 }
+//登录
+let loginPage = document.querySelector('#login-page'),
+    usernameInput = document.querySelector('#username'),
+    loginButton = document.querySelector('#login'),
+    callPage = document.querySelector('#call-page'),
+    theirnameInput = document.querySelector('#their-username'),
+    callButton = document.querySelector('#call'),
+    hangUpButton = document.querySelector('#hang-up');
+
+callPage.style.display = 'none';
+loginButton.addEventListener('click',(e)=>{
+    name = usernameInput.value;
+    if(name.length>0){
+        send({
+            type: 'login',
+            name: name,
+        });
+    }
+});
+function onLogin(success){
+    if(success === false){
+        alert('登录失败，请使用另一个名字');
+    }else{
+        loginPage.style.display = 'none';
+        callPage.style.display = 'block';
+        startConnection();
+    }
+}
